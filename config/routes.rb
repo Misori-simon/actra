867e8e38bee0f36a5ac1ac191b5f5418e9e3cadb
Misori-simon <misorisimon@gmail.com>
2021-02-17 10:45:07 +0100

    add the groups model - add crud controllers for models and views - add one to many relation between user and group

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :groups
   resources :users, only: %i[index show new create edit update]
   resource :session, only: %i[new create destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_100525) do
+ActiveRecord::Schema.define(version: 2021_02_16_153907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name"
+    t.string "icon"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
