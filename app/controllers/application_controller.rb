3fce677ad1d48fe348e455233edb3da542984b94
Misori-simon <misorisimon@gmail.com>
2021-03-01 09:10:59 +0100

    - Create methods to check if user has any competition or not. - Removed this checks from the views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,16 @@ class ApplicationController < ActionController::Base
   def logged_in?
     current_user
   end
+
+  def no_competiton_message
+    'No Competitions yet, add a competition' if @competitions.empty?
+  end
+
+  def no_group_message
+    'No groups yet, add a group' if @groups.empty?
+  end
+
+  def no_goal_message
+    'No goals yet, add a goal' if @goals.empty?
+  end
 end
