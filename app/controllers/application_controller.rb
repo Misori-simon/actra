edaa0973da0c8cdb5a38e368275040bd97e0bc1e
Misori-simon <misorisimon@gmail.com>
2021-03-14 11:45:43 +0100

    Add hepler methods, Remove logic from views - Remove logic from all form partials to a 'display_form_errors' helper - Remove logic in layout/header partial to a 'display_login_navigation' helper - Remove remove logic from layout/application to 'diplay_notice' and 'display_errors' helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?, :no_competiton_message, :no_group_message,
                 :no_goal_message, :group_absent?, :competition_absent?,
-                :read_date, :read_time
+                :read_date, :read_time, :display_form_errors, :display_login_navigation,
+                :display_notice, :display_errors
 
   private
 
@@ -35,5 +36,56 @@ class ApplicationController < ActionController::Base
 
   def read_date(date)
     date.strftime('%I:%M%p, %m/%d/%Y')
+  end
+
+  def display_form_errors(obj)
+    return unless obj.errors.any?
+
+    errors = "<div id='error_explanation'>"
+    errors += '<h2>'
+    errors += pluralize(obj.errors.count, 'error')
+    errors += 'prohibited this user from signing up:</h2>'
+    errors += '<ul>'
+
+    obj.errors.full_messages.each do |message|
+      errors += '<li>'
+      errors += message
+      errors += '</li>'
+    end
+    errors += '</ul>'
+    errors += '</div>'
+  end
+
+  def display_login_navigation
+    if logged_in?
+      nav = view_context.link_to current_user.name.capitalize.to_s, current_user
+      nav += view_context.link_to 'Logout', session_path, method: :delete, class: 'btn-ter'
+    else
+      nav = view_context.link_to('Sign Up', new_user_path, class: 'btn-ter')
+      nav += view_context.link_to('Log In', new_session_path, class: 'btn-ter')
+    end
+    nav
+  end
+
+  def display_notice
+    return unless notice.present?
+
+    output = "<div class='notice'>"
+    output += ' <p>'
+    output += notice
+    output += '</p>'
+    output += '</div>'
+    output
+  end
+
+  def display_errors
+    return unless alert.present?
+
+    output = "<div class='alert'>"
+    output += ' <p>'
+    output += alert
+    output += '</p>'
+    output += '</div>'
+    output
   end
 end
